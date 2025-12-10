e823c81ff30db6c7b2334842a7baa92f1dd0ee73
Farhad <178732550+f3rhd@users.noreply.github.com>
2025-12-10 22:20:51 +0400

    Update aliases.h

--- aliases.h
+++ aliases.h
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <vector>
 #include <memory>
+#include <string>
 
 auto constexpr no_label = 0;
 auto constexpr forward_label = 1;
@@ -17,7 +18,9 @@ using memory_addr_t = uint64_t;
 using offset_t = int64_t;
 using reg_id_t = uint8_t;
 using label_id_t = uint64_t;
+using branch_instruction_id_t = uint64_t;
 using program_t = std::vector<std::unique_ptr<instruction_t>>;
 using d_cache_t = std::unordered_map<memory_addr_t, data_t>;
 using reg_file_t = std::unordered_map<reg_id_t, data_t>;
-
+using branch_history_t = uint8_t;
+using branch_lookup_t = std::unordered_map<branch_instruction_id_t, std::pair<const std::string*,branch_history_t>>;
