953946d1cf24fc36ae7b524f7cb21443fd8c670a
Farhad <178732550+f3rhd@users.noreply.github.com>
2025-12-27 00:51:29 +0400

    gui

--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 #include "cpu/cpu.h"
 
 #include "gui/gui_renderer.h"
-#include "gui/gui_reg_file.h"
-#include "gui/gui_assembly.h"
-#include <thread>
-#include <chrono>
+#include "gui/gui_elements.h"
 
+#include <chrono>
+#include <iostream>
 int main(int argc, char** argv) {
 
     parser_t parser;
@@ -23,19 +22,51 @@ int main(int argc, char** argv) {
     gui::renderer::init("BranchPredict", 800, 600);
     GLFWwindow* window = gui::renderer::get_window();
 
-    gui::reg_file_element_t gui_reg_file(&cpu.get_reg_file());
-    gui::assembly_element_t gui_assembly(std::move(parse_result.second),&cpu.get_pc());
-
     if (!window)
         return 1;
 
-    while (!glfwWindowShouldClose(window)) {
-        if (!cpu.halt())
-            cpu.execute();
+	float cpu_speed = 0;
+    CPU_EXECUTION action = CPU_EXECUTION::STOP;
+    bool follow_pc = false;
+	double accumulator = 0.0;
+    auto last_time = std::chrono::high_resolution_clock::now();
+    const double MAX_IPS = 60.0; 
 
+
+    while (!glfwWindowShouldClose(window)) {
         gui::renderer::new_frame();
-        gui_assembly.draw(); 
-        gui_reg_file.draw(); 
+        ImGui::SetNextWindowPos({ 0,0 });
+        ImGui::SetNextWindowSize(gui::renderer::get_window_size());
+        if (ImGui::Begin("CPU Visual", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse)) {
+
+            gui::draw(cpu,parse_result.second,follow_pc,action,cpu_speed);
+            auto now = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> delta = now - last_time;
+            last_time = now;
+
+            switch (action) {
+            case CPU_EXECUTION::RUN:
+                accumulator += cpu_speed * MAX_IPS * delta.count();
+                while (accumulator >= 1.0 && !cpu.endofprogram()) {
+                    cpu.execute();
+                    accumulator -= 1.0;
+                }
+                break;
+            case CPU_EXECUTION::STEP:
+                if (!cpu.endofprogram())
+                    cpu.execute();
+                action = CPU_EXECUTION::STOP;
+                break;
+            case CPU_EXECUTION::RESET:
+                cpu.reset();
+                break;
+            case CPU_EXECUTION::STOP:
+            default:
+                break;
+
+            }
+            ImGui::End();
+        }
         gui::renderer::render_frame(window);
     }
 
